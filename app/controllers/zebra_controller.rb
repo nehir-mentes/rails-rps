431ac0b093b005a02230cc9a5763665c7d4a1541
nehir-mentes <nehirmen.98@gmail.com>
2025-04-21 20:44:46 +0000

    draft

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,5 +1,43 @@
 class ZebraController < ApplicationController
-  def giraffe
-    render{:plain => "howdy"}
+  
+  def homepage
+    render({:template => "zebra/homepage"})
+  end
+
+  def rock
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+    render :template => "game_templates/play_rock"
   end  
+
+  def paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    else
+      @outcome = "lost"
+    end
+    render :template => "game_templates/play_paper"
+  end
+
+  def scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    else
+      @outcome = "tied"
+    end
+    render :template => "game_templates/play_scissors"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  get("/rock", { :controller => "", :action => ""}
+  
+  get("/", { :controller => "zebra", :action => "homepage"})
+
+  get("/rock", { :controller => "zebra", :action => "rock"})
+
+  get("/paper", { :controller => "zebra", :action => "paper"})
+
+  get("/scissors", { :controller => "zebra", :action => "scissors"})
 end
